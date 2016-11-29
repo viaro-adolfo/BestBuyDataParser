5c511ea3223f4aed281b260e088c309b2aada8ff
Adolfo <adolfo.morales@viaro.net>
2016-11-29 10:05:56 -0600

    Init structure of parser

---
+++ Parser/parser.py
@@ -0,0 +1,5 @@
+class Parser(object):
+    """ This class will help us to parse data from JSON to CSV """
+    def __init__(self, arg):
+        super(Parser, self).__init__()
+        self.arg = arg
---
+++ Utilities/csvUtilies.py
@@ -0,0 +1,11 @@
+class CsvUtilities(object):
+    """docstring for CsvUtilities"""
+    def __init__(self, arg):
+        super(CsvUtilities, self).__init__()
+        self.arg = arg
+
+    def create_file(file_name=""):
+        return True
+
+    def add_data_to_file(data=[]):
+        return True
