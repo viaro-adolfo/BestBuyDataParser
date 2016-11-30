0e542a144c547c3b9233e2e8ab9b62f05e0e3a27
Adolfo <adolfo.morales@viaro.net>
2016-11-30 12:43:19 -0600

    Add functions code, not tested yet

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,5 +1,19 @@
+from Utilities.csvUtilies import CsvUtilities
+from Utilities.xmlUtilities import XmlUtilities
+import requests
+
+
 class Parser(object):
     """ This class will help us to parse data from JSON to CSV """
-    def __init__(self, arg):
+    csv_utilities = None
+    xml_utilities = None
+
+    def __init__(self):
         super(Parser, self).__init__()
-        self.arg = arg
+        self.csv_utilities = CsvUtilities()
+        self.xml_utilities = XmlUtilities()
+
+    def generate_reviews_csv_file(self, file_name):
+        csv_products = self.csv_utilities.read_file(file_name)
+        products_info = self.csv_utilities.get_product_name_and_sku(csv_products)
+        skus = ','.join(str(i[0]) for i in products_info)
---
+++ Utilities/xmlUtilities.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as ET
+
+
+class XmlUtilities(object):
+    """docstring for JsonUtilities"""
+
+    def get_xml_data(data=''):
+        root = ET.fromstring(data)
+        data = []
+        required_data = [
+            'rating',
+            'title',
+            'reviewer',
+            'comment',
+            'submissionTime'
+        ]
+
+        for child in root:
+            row_data = []
+
+            for attr in child:
+                tag = attr.tag
+                tag_val = attr.text
+
+                if tag not in required_data:
+                    continue
+
+                if tag == 'reviewer':
+                    tag_val = attr.find('name').text
+
+                row_data.append(tag_val)
+            data.append(row_data)
+        return data
